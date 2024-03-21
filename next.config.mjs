7b29d0e83cdc0ea9a028b944f115428449c3d2dc
RuslanB <menejer.r@yandex.com>
2024-03-21 15:45:21 +0300

    fix config

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-        port: '',
-        pathname: '',
-      },
-    ],
-  },};
+const nextConfig = {};
 
 export default nextConfig;
+
